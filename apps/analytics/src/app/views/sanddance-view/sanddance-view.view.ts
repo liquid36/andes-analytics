5ffa3d521f665268696342699c1b355fd6fa2c07
Mariano Andres Botta <marianoabotta@gmail.com>
2019-11-21 14:22:35 -0300

    feat(sanddance): graficos predefinidos

--- apps/analytics/src/app/views/sanddance-view/sanddance-view.view.ts
+++ apps/analytics/src/app/views/sanddance-view/sanddance-view.view.ts
@@ -21,6 +21,8 @@ export class AppSandDanceView implements AfterViewInit {
     public prestaciones$;
     public data$;
 
+    public cacheData = null;
+
     constructor(
         private snomed: SnomedAPI,
         private activeRoute: ActivatedRoute,
@@ -56,11 +58,94 @@ export class AppSandDanceView implements AfterViewInit {
                 })
             })
         ).subscribe((m) => {
+            this.cacheData = m;
             setTimeout(() => {
+
                 this.changeDetector.detectChanges();
-                this.element.nativeElement.contentWindow.postMessage(m, '*');
+                this.element.nativeElement.contentWindow.postMessage({
+                    data: m,
+                    insight: piramidaPoblacional
+                }, '*');
+                // setInterval(() => {
+                //     this.element.nativeElement.contentWindow.postMessage({
+                //         action: 'getInsight'
+                //     }, '*');
+                // }, 2000)
+
+                // window.addEventListener('message', e => {
+                //     if (e.data) {
+                //         const data = e.data;
+                //         if (data.request.action === 'getInsight') {
+                //             console.log(JSON.stringify(data.insight));
+                //         }
+                //     }
+                // });
             }, 500)
         })
 
     }
+
+    changeInsight(grafico) {
+        this.changeDetector.detectChanges();
+        this.element.nativeElement.contentWindow.postMessage({
+            data: this.cacheData,
+            insight: grafico === 'piramide' ? piramidaPoblacional : fechaXedad3D
+        }, '*');
+    }
 }
+
+
+const piramidaPoblacional = {
+    "colorBin": "quantile",
+    "columns": {
+        "x": "concepto",
+        "sort": "edad",
+        "color": "edad",
+        "size": "edad",
+        "z": "edad",
+        "y": "edad",
+        "facet": "sexo"
+    },
+    "facets": {
+        "columns": 2,
+        "rows": 1
+    },
+    "filter": [
+        {
+            "key": 0,
+            "clause": null,
+            "expressions": [
+                {
+                    "key": 0,
+                    "clause": null,
+                    "name": "edad",
+                    "operator": "<",
+                    "value": "100",
+                    "unlocked": true,
+                    "errorMessage": null
+                }
+            ]
+        }
+    ],
+    "hideAxes": false,
+    "hideLegend": false,
+    "scheme": "redyellowgreen",
+    "signalValues": {
+        "RoleZ_ProportionSignal": 0.6,
+        "Text_ScaleSignal": 2,
+        "Text_AngleXSignal": 30,
+        "Text_AngleYSignal": 0,
+        "Mark_OpacitySignal": 1,
+        "RoleY_BinsSignal": 20,
+        "RoleColor_BinCountSignal": 7,
+        "RoleColor_ReverseSignal": false
+    },
+    "size": {
+        "height": 439,
+        "width": 1470
+    },
+    "chart": "barchartH",
+    "view": "2d"
+}
+
+const fechaXedad3D = { "colorBin": "quantile", "columns": { "x": "fecha", "sort": "edad", "color": "fecha", "size": "edad", "z": "edad", "y": "edad" }, "directColor": false, "facets": { "columns": 2, "rows": 1 }, "filter": [{ "key": 0, "clause": null, "expressions": [{ "key": 0, "clause": null, "name": "edad", "operator": "<", "value": "100", "unlocked": true, "errorMessage": null }] }, { "key": 0, "clause": "&&", "expressions": [{ "key": 0, "clause": null, "name": "edad", "operator": "<", "value": "100", "unlocked": true, "errorMessage": null }] }], "hideAxes": false, "hideLegend": false, "scheme": "category20", "signalValues": { "RoleZ_ProportionSignal": 0.6, "Text_ScaleSignal": 2, "Text_AngleXSignal": 30, "Text_AngleYSignal": 0, "Mark_OpacitySignal": 1, "RoleColor_BinCountSignal": 7, "RoleColor_ReverseSignal": false }, "size": { "height": 853, "width": 1470 }, "chart": "barchartV", "view": "3d" }
