330b61b9b39456ebe6c5803c57a9011cf300b246
Mariano Andres Botta <marianoabotta@gmail.com>
2019-12-09 08:54:17 -0300

    feat(express): update to @andes/api-tool/bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,87 +1,21 @@
 
+import { ApiBootstrap } from '@andes/api-tool/build/bootstrap';
+
 require('dotenv').config();
 
-const express = require('express');
-// const logger = require('morgan');
-const bodyParser = require('body-parser');
-
 const snomed = require('./app/routes/snomed').router;
-// const expressions = require('./app/routes/expressions');
 const andes = require('./app/routes/andes').router;
 
-const accessControlConfig = {
-    "allowOrigin": "*",
-    "allowMethods": "GET,POST,PUT,DELETE,HEAD,OPTIONS"
-};
-
-//  ************************
-
-var app = express();
-
-// app.use(logger('dev'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(function (req, res, next) {
-    var oneof = false;
-    if (req.headers.origin) {
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        oneof = true;
-    }
-    if (req.headers['access-control-request-method']) {
-        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
-        oneof = true;
-    }
-    if (req.headers['access-control-request-headers']) {
-        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
-        oneof = true;
-    }
-    if (oneof) {
-        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
-    }
-
-    // intercept OPTIONS method
-    if (oneof && req.method === 'OPTIONS') {
-        return res.send(200);
-    } else {
-        return next();
-    }
-});
-
-app.use('/api/snomed', snomed);
-app.use('/api/andes', andes);
-// app.use("/api/expressions", expressions);
-
-/// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    const err: any = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.status(err.status >= 100 && err.status < 600 ? err.code : 500).send(err.message);
-    });
+const info = {
+    name: 'andes-analytics',
+    version: '1.0.0'
 }
-
-// production error handler
-// no stacktraces leaked to user
-// Adding raw body support
-app.use(function (err, req, res, next) {
-    res.status(err.status >= 100 && err.status < 600 ? err.code : 500).send(err.message);
-});
-
-const cluster = require('cluster');
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT) || 3000;
 const host = process.env.HOST || '0.0.0.0';
+const key = '8XOcgoMojZE4CwhluNkM9FtwzCfUPJWhf7'
+const application = new ApiBootstrap(info, { port, host, key });
 
-const server = app.listen(port, host, function () {
-    console.log('Process ' + process.pid + ' is listening in port ' + port + ' to all incoming requests');
-});
+application.add({ path: '/api/snomed', router: snomed });
+application.add({ path: '/api/andes', router: andes });
+
+application.start();
